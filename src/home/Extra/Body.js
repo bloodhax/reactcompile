3ebd9fd5c8efef79b2047d97f6342b08d47b730c
lacsinagramar <lacsinagramar@gmail.com>
2017-12-03 06:44:59 +0800

    link fixes

--- src/home/Extra/Body.js
+++ src/home/Extra/Body.js
@@ -109,7 +109,7 @@ class Body extends Component {
                               </div>
                             </div>
                             <div className="vc_empty_space" style={{height: 30}}><span className="vc_empty_space_inner" /></div>
-                            <a className="btn btn-dark-solid btn-small " href="signup.html">SIGN UP NOW!</a></div></div></div></div></div></div><div className="vc_row-full-width vc_clearfix" /><div data-vc-full-width="true" data-vc-full-width-init="false" className="vc_row wpb_row vc_row-fluid vc_custom_1453540542511"><div className="wpb_column vc_column_container vc_col-sm-12"><div className="vc_column-inner "><div className="wpb_wrapper"><div className="text-center">
+                            <a className="btn btn-dark-solid btn-small " href="/signup">SIGN UP NOW!</a></div></div></div></div></div></div><div className="vc_row-full-width vc_clearfix" /><div data-vc-full-width="true" data-vc-full-width-init="false" className="vc_row wpb_row vc_row-fluid vc_custom_1453540542511"><div className="wpb_column vc_column_container vc_col-sm-12"><div className="vc_column-inner "><div className="wpb_wrapper"><div className="text-center">
                         </div>
                       </div></div></div></div>
                 <div className="divider d-solid d-single Massivea13480450eebef51eac76f0e0489bf5b" role="divider" />
